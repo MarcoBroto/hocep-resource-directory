f98bd05f462f7c82bdd92a025f8392f0b55aea2e
MarcoBroto <msoto9940@gmail.com>
2019-04-22 23:39:02 -0600

    Finished main views

--- SQL Views/createViews.sql
+++ SQL Views/createViews.sql
@@ -3,76 +3,83 @@
 #### Listing Views ####
 
 # Get list of names of all available resources. Used for front-end selection menus.
-CREATE VIEW resource_list AS (
-	SELECT name FROM Resource ORDER BY name
+CREATE OR REPLACE VIEW `resource_list` AS (
+	SELECT title FROM Resource ORDER BY title
 );
 
 # Get list of names of all available zipcodes. Used for front-end selection menus.
-CREATE VIEW zipcode_list AS (
-	SELECT distinct(zipcode) FROM Resource ORDER BY zipcode
+CREATE OR REPLACE VIEW `zipcode_list` AS (
+	SELECT distinct(zipcode) FROM resource ORDER BY zipcode
 );
 
 # Get list of names of all available categories. Used for front-end selection menus.
-CREATE VIEW category_list AS (
-	SELECT name FROM Category ORDER BY name
+CREATE OR REPLACE VIEW `category_list` AS (
+	SELECT name FROM category ORDER BY name
 );
 
 # Get list of names of all available services. Used for front-end selection menus. 
-CREATE VIEW service_list AS (
-	SELECT name FROM Service ORDER BY name
+CREATE OR REPLACE VIEW `service_list` AS (
+	SELECT name FROM service ORDER BY name
 );
 
 ######################
 ### Grouping Views ###
 
 # Gets list of Admins and login credentials. Used for login page validation.
-CREATE VIEW admin_logins AS (
+CREATE OR REPLACE VIEW `admin_logins` AS (
 	SELECT username, password FROM Admin
 );
 
 # Merge all categories associated with each resource into JSON array. Join into edit_rows and search_rows views.
-CREATE VIEW category_grouped AS (
-	SELECT resource_id, JSON_ARRAYAGG(name) AS 'categories' FROM Category GROUP BY resource_id
+CREATE OR REPLACE VIEW `category_grouped` AS (
+	SELECT resource_id, JSON_ARRAYAGG(name) AS 'categories' FROM category
+	JOIN belongs_to ON category.category_id=belongs_to.category_id 
+	GROUP BY resource_id
 );
 
 # Merge all services associated with each resource into JSON array. Join into edit_rows and search_rows views.
-CREATE VIEW service_grouped AS (
-	SELECT resource_id, JSON_ARRAYAGG(name) AS 'services' FROM Service GROUP BY resource_id
+CREATE OR REPLACE VIEW `service_grouped` AS (
+	SELECT resource_id, JSON_ARRAYAGG(name) AS 'services' FROM service
+	JOIN provides ON service.service_id=provides.service_id
+	GROUP BY resource_id
 );
 
 # Create JSON objects from each field of a contact. Will be merged into array in contact_grouped view.
-CREATE VIEW contact_objects AS (
-	# TODO: Query to merge all data from each contact into its own JSON object.
-)
+CREATE OR REPLACE VIEW `contact_objects` AS (
+	SELECT resource_id, json_object('contact_id',contact_id, 'title',title, 'f_name',f_name, 'l_name',l_name,'phone',phone, 'email',email) AS contact FROM contact
+);
 
 # Merge all contact JSON objects into JSON array. Will be joined into data row views. 
-CREATE VIEW contact_grouped AS (
-	# TODO: Query to merge all contact json objects into grouped json array.
+CREATE OR REPLACE VIEW `contact_grouped` AS (
+	SELECT resource_id, JSON_ARRAYAGG(contact) AS contacts FROM contact_objects
+	GROUP BY resource_id
 );
 
 # Merge Updates and Admin tables to be used to merge into edit_row view
-CREATE VIEW updates_w_admin AS (
-	SELECT Updates.admin_id, Updates.lastUpdate,  Admin.username FROM Updates
-		JOIN Admin ON Updates.admin_id=Admin.admin_id
+CREATE OR REPLACE VIEW `updates_w_admin` AS (
+	SELECT updates.admin_id as admin_id, resource_id, username, f_name, l_name, updates.date FROM updates
+	JOIN admin ON updates.admin_id=admin.admin_id
 );
 
 ##########################
 ### Data Display Views ###
 
 # View for Resource Editor app, must have all data associated with each row
-CREATE VIEW edit_rows AS (
-	SELECT * FROM Resource ORDER BY name
-		JOIN category_grouped ON Resource.resource_id=category_grouped.resource_id 
-		JOIN service_grouped ON Resource.resource_id=service_grouped.resource_id
-		# TODO: Join contact_grouped table
+CREATE OR REPLACE VIEW `editor_rows` AS (
+	SELECT resource.resource_id, title AS name, categories, services, street_address, zipcode, phone, email, website, description, documents, requirements, insurance, hoursOfOp AS opHours, contacts, admin_id, username AS lastUpdate_admin, updates_w_admin.date as lastUpdate FROM resource
+	JOIN category_grouped ON resource.resource_id=category_grouped.resource_id
+	JOIN service_grouped ON resource.resource_id=service_grouped.resource_id
+	JOIN contact_grouped ON resource.resource_id=contact_grouped.resource_id
+	JOIN updates_w_admin ON resource.resource_id=updates_w_admin.resource_id
+	ORDER BY title
 );
 
 # View for Search Page app, must have all data associated with each row except for the username of the  admin who updated the document.
-CREATE VIEW search_rows AS (
-	SELECT name, street, zipcode, phone, email, website, description,
-		requirements, documents, insurance, opHours  FROM Resource ORDER BY name 
-		JOIN category_grouped ON Resource.resource_id=category_grouped.resource_id 
-		JOIN service_grouped ON Resource.resource_id=service_grouped.resource_id 
-		JOIN Updates ON Resource.resource_id=Updates.resource_id
-		# TODO: Join contact_grouped table
+CREATE OR REPLACE VIEW `search_rows` AS (
+	SELECT resource.resource_id, title AS name, categories, services, street_address, zipcode, phone, email, website, description, documents, requirements, insurance, hoursOfOp AS opHours, contacts, updates.date AS lastUpdate FROM resource
+	JOIN category_grouped ON resource.resource_id=category_grouped.resource_id
+	JOIN service_grouped ON resource.resource_id=service_grouped.resource_id
+	JOIN contact_grouped ON resource.resource_id=contact_grouped.resource_id
+	JOIN updates ON resource.resource_id=updates.resource_id
+	ORDER BY title
 );
