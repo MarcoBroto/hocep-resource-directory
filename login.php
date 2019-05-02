80b96f2dcd06313a4a125cc4ab2543522e5f1fcb
jagomez21 <jagomez21@miners.utep.edu>
2019-05-02 10:43:20 -0600

    Requested Changes.
    > login.php - Action from login form was removed, this will be handled in another language.
    
    + checkLogin.php - This class checks and validates if the given credentials (username and password) are valid/correct.

--- login.php
+++ login.php
@@ -1,51 +1,12 @@
-<?php
-    include("php/config.php");
-
-    session_start();
-
-    if ($db->connect_error) {
-        die('Connection Error (' . $db->connect_errno . ') ' . $db->connect_error);
-    }
-
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $username = mysqli_real_escape_string($db, $_POST['username']);
-        $password = mysqli_real_escape_string($db, $_POST['password']);
-
-        //Building the query
-        $stmt = $db -> prepare("SELECT * FROM Admin WHERE Username = ?");
-        $stmt -> bind_param("s", $username);
-        $stmt -> execute();
-        $result = $stmt -> get_result();
-
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-        if (count($row) > 0) {
-            if ($password == $row['Password']) {
-                $_SESSION['login_user'] = $row['Username'];
-                $_SESSION['login_id'] = $row['Admin_id'];
-                header("location: edit/index.php");
-            } else {
-                $error = "Invalid password.";
-            }
-        } else {
-            $error = "Invalid username or password.";
-        }
-        $stmt->free_result();
-        $stmt->close();
-    }
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Admin Login</title>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="robots" content="all,follow">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
 
 	<!-- Bootstrap with Flatly Theme import -->
 	<link href="bootstrap/flatly.bootstrap.min.css" rel="stylesheet">
@@ -76,9 +37,9 @@
 	</div>
 	<hr/>
 	<div class="container" style="width: 45vw;">
-        <p style="color:#ff0000"><?php echo $error; ?></p>
-        <form id="login-form" method="post" action="login.php">
-			<fieldset>
+        <!-- <form id="login-form" action="php/checkLogin.php" method="POST" > -->
+        <form id="login-form" action="" method="POST" >
+        <fieldset>
 				<legend><label>Username</label></legend>
 				<input v-if="failed" type="form-text" class="form-control form-control-lg is-invalid" name="username" aria-describedby="username" required>
 				<input v-else type="form-text" class="form-control form-control-lg" name="username" aria-describedby="username" required>
