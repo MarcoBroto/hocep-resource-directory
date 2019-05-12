782944d9f7164c5f4a09443b845c5a0dd242e9b4
MarcoBroto <msoto9940@gmail.com>
2019-05-11 19:56:38 -0600

    Removed default json parse option
    editor.js
    - Removed default json parse option from ajax requests and wrapped the handler in a trycatch block
    
    fetchMultiselectOptions.js
    - Matches search functionality branch version.

--- js/editor.js
+++ js/editor.js
@@ -2,50 +2,64 @@
 const request = require('ajax-request');
 
 export function createResource(resource_data) {
-	request({url: '../php/createResource.php', method: 'POST', data: {'resource': resource_data}, json: true}, function(err, response, body) {
+	request({url: '../php/createResource.php', method: 'POST', data: {'resource': resource_data}}, function(err, response, body) {
 		if (err) {
 			console.log("CREATE RESOURCE REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Added Resource Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			body = JSON.parse(body);
+			if (body.response) {
+				console.log('Added Resource Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Create Resource.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Create Resource.');
 	});
 }
 
 export function updateResource(resource_data) {
-	request({url: '../php/updateResource.php', method: 'POST', data: {'resource': resource_data}, json: true}, function(err, response, body) {
+	request({url: '../php/updateResource.php', method: 'POST', data: {'resource': resource_data}}, function(err, response, body) {
 		if (err) {
 			console.log("UPDATE RESOURCE REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Updated Resource Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			if (body.response) {
+				console.log('Updated Resource Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Update Resource.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Update Resource.');
 	});
 }
 
 export function deleteResource(resource_id) {
-	request({url: '../php/deleteResource.php', method: 'POST', data: {'id': resource_id}, json: true}, function(err, response, body) {
+	request({url: '../php/deleteResource.php', method: 'POST', data: {'id': resource_id}}, function(err, response, body) {
 		if (err) {
 			console.log("DELETE RESOURCE REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Deleted Resource Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			body = JSON.parse(body);
+			if (body.response) {
+				console.log('Deleted Resource Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Delete Resource.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Delete Resource.');
 	});
 }
 
@@ -55,18 +69,23 @@ export function createTag(tag_type, tag_data) {
 	if (tag_type !== 'category' && tag_type !== 'service') {
 		console.log("Create Tag Failed: Invalid Tag Type")
 	}
-	request({url: '../php/createTag.php', method: 'POST', data: {'type': tag_type, 'tag': tag_data}, json: true}, function(err, response, body) {
+	request({url: '../php/createTag.php', method: 'POST', data: {'type': tag_type, 'tag': tag_data}}, function(err, response, body) {
 		if (err) {
 			console.log("CREATE TAG REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Added Tag Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			body = JSON.parse(body);
+			if (body.response) {
+				console.log('Added Tag Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Create Tag.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Create Tag.');
 	});
 }
 
@@ -74,18 +93,23 @@ export function updateTag(tag_type, tag_data) {
 	if (tag_type !== 'category' && tag_type !== 'service') {
 		console.log("Update Tag Failed: Invalid Tag Type")
 	}
-	request({url: '../php/updateTag.php', method: 'POST', data: {'type': tag_type, 'tag': tag_data}, json: true}, function(err, response, body) {
+	request({url: '../php/updateTag.php', method: 'POST', data: {'type': tag_type, 'tag': tag_data}}, function(err, response, body) {
 		if (err) {
 			console.log("UPDATE TAG REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Updated Tag Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			body = JSON.parse(body);
+			if (body.response) {
+				console.log('Updated Tag Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Update Tag.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Update Tag.');
 	});
 }
 
@@ -93,17 +117,22 @@ export function deleteTag(tag_type, tag_id) {
 	if (tag_type != 'category' || tag_type != 'service') {
 		console.log("Delete Tag Failed: Invalid Tag Type")
 	}
-	request({url: '../php/deleteTag.php', method: 'POST', data: {'type': tag_type, 'id': tag_id}, json: true}, function(err, response, body) {
+	request({url: '../php/deleteTag.php', method: 'POST', data: {'type': tag_type, 'id': tag_id}}, function(err, response, body) {
 		if (err) {
 			console.log("DELETE TAG REQUEST ERROR");
 			return;
 		}
 		console.log(body);
-		if (body.response) {
-			console.log('Deleted Tag Successfully.');
-			// TODO: refresh edit page interface items
+		try {
+			body = JSON.parse(body);
+			if (body.response) {
+				console.log('Deleted Tag Successfully.');
+				// TODO: refresh edit page interface items
+			}
+			else
+				console.log('Failed to Delete Tag.');
+		} catch(err) {
+			console.log(err);
 		}
-		else
-			console.log('Failed to Delete Tag.');
 	});
 }
