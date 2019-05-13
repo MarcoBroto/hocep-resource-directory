aa4c30d0e4c60ca306b7248af8839030e45ae7e1
MarcoBroto <msoto9940@gmail.com>
2019-05-13 14:12:17 -0600

    Re-removed test directory, updated bundle files
    NOTE: Do not include test directory in new commits and merges.

--- test/Category.js
+++
@@ -1,8 +0,0 @@
-
-class Category {
-	constructor(category_id, category_name, category_descrip) {
-		this.id = category_id;
-		this.name = category_name;
-		this.description = category_descrip;
-	}
-}
--- test/Service.js
+++
@@ -1,8 +0,0 @@
-
-class Service {
-	constructor(service_id, service_name, service_descrip) {
-		this.id = service_id;
-		this.name = service_name;
-		this.description = service_descrip;
-	}
-}
